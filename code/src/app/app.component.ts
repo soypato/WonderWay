1ebfc299f9b55519c9dd2c009a3eb6144a25b9d9
soypato <patriciotubio@gmail.com>
2024-10-21 12:32:01 -0300

    Add firsts components

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormLoginComponent } from './loginPage/form-login/form-login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
