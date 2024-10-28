59b8adb8e1ca94dbcc9452e7f542d431d0c855d9
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2024-10-28 00:11:36 -0300

    added navbar

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormLoginComponent } from './loginPage/form-login/form-login.component';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormLoginComponent],
+  imports: [RouterOutlet, FormLoginComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
