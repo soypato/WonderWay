80951848dfba5fa661d249c6d54d75edee468af2
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2024-10-27 23:59:49 -0300

    Forms now are reactive

--- code/src/app/loginPage/form-login/form-login.component.ts
+++ code/src/app/loginPage/form-login/form-login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-login',
   standalone: true,
-  imports: [],
   templateUrl: './form-login.component.html',
-  styleUrl: './form-login.component.css'
+  styleUrls: ['./form-login.component.css'],
+  imports: [ReactiveFormsModule]
 })
-export class FormLoginComponent {
+export class FormLoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+     this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],  // Control para el correo electrónico
+      password: ['', [Validators.required]]                   // para la contraseña
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado', this.loginForm.value);
+      // LOGICA DE LOGIN ACA
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
--- code/src/app/registerPage/form-register/form-register.component.ts
+++ code/src/app/registerPage/form-register/form-register.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
   standalone: true,
-  imports: [],
   templateUrl: './form-register.component.html',
-  styleUrl: './form-register.component.css'
+  styleUrls: ['./form-register.component.css'],
+  imports: [ReactiveFormsModule] 
 })
-export class FormRegisterComponent {
-  
+export class FormRegisterComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],                       // Control para el nombre
+      email: ['', [Validators.required, Validators.email]],   //  para el correo electrónico
+      password: ['', [Validators.required, Validators.minLength(6)]], //  para la contraseña
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Formulario de registro enviado', this.registerForm.value);
+      // LOGICA PARA EL REGISTRO, QUE VAYA ACA
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
