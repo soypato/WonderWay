8c59b563e8b162f1eebc3734762b8bdbe8b3cb6f
JuanC07 <juanmciardi@gmail.com>
2024-10-31 14:40:37 -0300

    add services folder

--- code/src/app/registerPage/form-register/form-register.component.ts
+++ code/src/app/registerPage/form-register/form-register.component.ts
@@ -1,3 +1,4 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
@@ -6,7 +7,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractContro
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule] 
+  imports: [ReactiveFormsModule, NgIf] 
 })
 export class FormRegisterComponent implements OnInit {
   registerForm: FormGroup;
