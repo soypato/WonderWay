72f8c970f4e1947ade400ae5b43f84d2a54831fc
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2024-11-13 01:10:16 -0300

    first version api files

--- code/src/app/components/travel-modes/freemode/freemode.component.ts
+++ code/src/app/components/travel-modes/freemode/freemode.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Inject, OnInit } from "@angular/core";
+import { TripadvisorService } from "../../../services/tripadvisor.service";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+interface FlightData {
+}
+
+interface HotelData {
+}
+
+interface RestaurantData {
+}
 
 @Component({
-  selector: 'app-freemode',
-  standalone: true,
-  imports: [],
+  selector: 'app-example',
   templateUrl: './freemode.component.html',
-  styleUrl: './freemode.component.css'
+  styleUrls: ['./freemode.component.css']
 })
-export class FreemodeComponent {
+export class ExampleComponent implements OnInit {
 
+
+  tripAdvisorService = Inject(TripadvisorService);
+  flightsData: FlightData[] | undefined;
+  hotelsData: HotelData[] | undefined;
+  restaurantsData: RestaurantData[] | undefined;
+
+  ngOnInit(): void {
+      // Example call to search for flights
+      this.tripAdvisorService.searchFlights('BOM', 'DEL').subscribe({
+        next: (data: FlightData[]) => { 
+          console.log('Flight Data:', data);
+          this.flightsData = data;
+        },
+        error: (error: HttpErrorResponse) => {  // Using HttpErrorResponse
+          console.error('Flight search error:', error.message);
+        }
+      });
+    
+      // Example call to search for hotels
+      this.tripAdvisorService.searchHotels('mumbai').subscribe({
+        next: (data: HotelData[]) => { 
+          console.log('Hotel Data:', data);
+          this.hotelsData = data;
+        },
+        error: (error: HttpErrorResponse) => {  // Using HttpErrorResponse
+          console.error('Hotel search error:', error.message);
+        }
+      });
+    
+      // Example call to search for restaurants
+      this.tripAdvisorService.searchRestaurants('mumbai').subscribe({
+        next: (data: RestaurantData[]) => { 
+          console.log('Restaurant Data:', data);
+          this.restaurantsData = data;
+        },
+        error: (error: HttpErrorResponse) => {  // Using HttpErrorResponse
+          console.error('Restaurant search error:', error.message);
+        }
+      });
+  }
 }
--- code/src/app/services/tripadvisor.service.ts
+++ code/src/app/services/tripadvisor.service.ts
@@ -1,13 +1,51 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environments';
+import { HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripadvisorService {
+  http: any;
 
   constructor() { }
-  url = 'https://tripadvisor16.p.rapidapi.com/api/v1/hotels/searchLocation';
+  baseUrl = 'https://tripadvisor16.p.rapidapi.com/api/v1/hotels/searchLocation';
+  apiKey = environment.rapidApiKey;
+  private headers = new HttpHeaders({
+    'x-rapidapi-key': environment.rapidApiKey,
+    'x-rapidapi-host': 'tripadvisor16.p.rapidapi.com'
+  });
 
-  
+   // Método para buscar vuelos
+   searchFlights(source: string, destination: string): Observable<any> {
+    const url = `${this.baseUrl}/flights/searchFlights?sourceAirportCode=${source}&destinationAirportCode=${destination}&itineraryType=ONE_WAY&sortOrder=ML_BEST_VALUE&numAdults=1&numSeniors=0&classOfService=ECONOMY&pageNumber=1&nearby=yes&nonstop=yes&currencyCode=USD&region=USA`;
+    return this.http.get(url, { headers: this.headers }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Método para buscar hoteles
+  searchHotels(query: string): Observable<any> {
+    const url = `${this.baseUrl}/hotels/searchLocation?query=${query}`;
+    return this.http.get(url, { headers: this.headers }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Método para buscar restaurantes
+  searchRestaurants(query: string): Observable<any> {
+    const url = `${this.baseUrl}/restaurant/searchLocation?query=${query}`;
+    return this.http.get(url, { headers: this.headers }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Método para manejar errores
+  private handleError(error: any): Observable<never> {
+    console.error('Ocurrió un error:', error);
+    return throwError(() => new Error(error.message || 'Error en el servicio'));
+  }
+
 
 }
