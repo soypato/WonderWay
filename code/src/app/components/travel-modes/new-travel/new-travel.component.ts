aa1f50ac7597e8a631b0755cedf00f73a5f67b9b
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2024-11-13 02:34:35 -0300

    feat. form new t.

--- code/src/app/components/travel-modes/new-travel/new-travel.component.ts
+++ code/src/app/components/travel-modes/new-travel/new-travel.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-travel',
-  standalone: true,
-  imports: [],
   templateUrl: './new-travel.component.html',
-  styleUrl: './new-travel.component.css'
+  styleUrls: ['./new-travel.component.css'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule]
 })
-export class NewTravelComponent {
+export class NewTravelComponent implements OnInit {
+  travelForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.travelForm = this.fb.group({
+      startDate: ['', Validators.required],
+      location: ['', Validators.required]
+    });
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
+  onSubmit() {
+    if (this.travelForm.valid) {
+      console.log('Form Submitted!', this.travelForm.value);
+    }
+  }
 }
