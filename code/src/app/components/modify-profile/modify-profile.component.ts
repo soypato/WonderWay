f7569ce963d4989d5e99b7322acb7634faa64b56
Ruzito-San <enzoguerci13@gmail.com>
2024-11-14 23:54:41 -0300

    at last i think its done

--- code/src/app/components/modify-profile/modify-profile.component.ts
+++ code/src/app/components/modify-profile/modify-profile.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractContro
 import { UserService } from '../../services/user.service';
 import { CurrentUser } from '../../services/current-user.service';
 import { User } from '../../interface/user.interface';
+import { InvokeFunctionExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-modify-profile',
@@ -13,31 +14,54 @@ import { User } from '../../interface/user.interface';
   styleUrls: ['./modify-profile.component.css']
 })
 export class ModifyProfileComponent implements OnInit {
-
   profileForm: FormGroup;
   passwordForm: FormGroup;
 
-  usuarioActual = inject(CurrentUser).getUsuario();
+  userid : number= 0; // te trae el id de la sesion actual -- es un number
+  currentUser : User | null = null; // creo var para guardar el user entero de la sesion actual
 
 
-  constructor(private fb: FormBuilder) {
+
+  constructor(private fb: FormBuilder,private userService : UserService) {
     // Formulario para nombre y email
     this.profileForm = this.fb.group({
       name: [''],
       email: ['', [Validators.email]],
-      currentPassword: ['', [Validators.required]] // contraseña actual
+      currentPassword: ['', [Validators.required]]
     });
-
     // Formulario para cambio de contraseña
     this.passwordForm = this.fb.group({
       currentPassword: [''],
-      newPassword: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', Validators.required]
+      newPassword: ['', [Validators.required], [Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]]
     });
   }
+
+  ngOnInit() {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile() {
+    this.userService.getUserProfile(this.userid).subscribe({
+      next: (user:User) => {
+        this.currentUser = user;
+        /*
+        console.log('Perfil del usuario cargado:', this.currentUser);
+        // Accede al valor de password
+        const userPassword = this.currentUser.password;
+        //console.log('Contraseña del usuario:', userPassword);
+        //ni ganan de mostrar tu contra con solo apretar f12
+        // Puedes usar `userPassword` aquí como desees*/
+      },
+      error: (err:Error) => {
+        console.error('Error al cargar el perfil del usuario:', err);
+      }
+    });
+  }
+
   // Método para validar que las nuevas contraseñas coincidan
   prevPasswordsMatch() {
-    if(this.profileForm.get('currentPassword')?.value === this.usuarioActual.password)
+    if(this.profileForm.get('currentPassword')?.value  === this.currentUser?.password  )
       {
         return true;
       }
@@ -51,7 +75,6 @@ export class ModifyProfileComponent implements OnInit {
     return false;
   }
 
-  ngOnInit(): void {}
 
   onProfileSubmit() {
     if (this.profileForm.valid) {
@@ -65,9 +88,5 @@ export class ModifyProfileComponent implements OnInit {
       console.error('Las contraseñas no coinciden');
     }
   }
-
-
-
-
 
 }
