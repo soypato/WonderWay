d8bacb805c6fd256f167c2a7c1eaa2fe1a012307
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2024-11-16 16:48:58 -0300

    navbar changes

--- code/src/app/components/navbar/navbar.component.ts
+++ code/src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { CurrentUser } from '../../services/current-user.service';
 import { Unsubscribable } from 'rxjs';
 import { User } from '../../interface/user.interface';
 import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -14,11 +15,19 @@ import { CommonModule } from '@angular/common';
 export class NavbarComponent implements OnInit{
 
   currentUserService = inject(CurrentUser);
+  userService = inject(UserService);
   currentUser: Number | null = null;
+  miPerfil : string | null = null;
 
   ngOnInit() : void
   {
     this.currentUser = this.currentUserService.getUsuario();
+    this.userService.getUserProfile(Number(this.currentUser)).subscribe(
+      {
+        next: (user: User) => {
+          this.miPerfil = user.name;
+        }
+      }
+    );
   }
-
 }
