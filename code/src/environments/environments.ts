4cdaa7423216eb48dc59b2dfa9517a1bedbe441d
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2024-11-20 22:51:12 -0300

    clear environments

--- code/src/environments/environments.ts
+++
@@ -1,9 +0,0 @@
-// Estos son datos preliminados que se pueden usar para probar la aplicación.
-// Están en GitHub, pero no pasa nada porque son datos de prueba.
-
-export const environment = {
-    production: false,
-    rapidApiKey: '07407c9188msh41459c49bbd4e21p18651bjsna414d3178beb',
-    keyPass: 'e23erf423e4r4ewf34rt45y56nrfbniu98'
-  };
-  
