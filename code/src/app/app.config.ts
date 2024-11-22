bcaf385ac3e1ad54f4172a0ca6ffca9b5a981dc4
Ruzito-San <enzoguerci13@gmail.com>
2024-11-21 23:13:08 -0300

    asdasd

--- code/src/app/app.config.ts
+++ code/src/app/app.config.ts
@@ -4,6 +4,14 @@ import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { routes } from './app.routes';
 import { HttpRequest, provideHttpClient } from '@angular/common/http';
 
+
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './loading.interceptor';
+
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideHttpClient(), SweetAlert2Module]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideHttpClient(), SweetAlert2Module,
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }]
 };
